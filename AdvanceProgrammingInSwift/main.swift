84c2c26ec8b46340313b456080fd163aab6d38a8
luu-9798 <trungluu9798@gmail.com>
2025-05-02 23:09:31 -0400

    Create a new constant to hold one of the enumeration cases

--- AdvanceProgrammingInSwift/main.swift
+++ AdvanceProgrammingInSwift/main.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
+//Define enum with raw values
 enum Ingredient: String {
     case lettuce = "Romaine Lettuce"
     case croutons = "Salted Croutons"
     case chicken = "Free Range Chicken"
 }
+
+//Create a new constant to hold one of the enumeration cases
+let ingredient: Ingredient = .lettuce
 
