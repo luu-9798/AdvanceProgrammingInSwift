8ccc28090e76bb6e9a9800050ec6485c5d0fd2c3
luu-9798 <trungluu9798@gmail.com>
2025-05-07 23:08:15 -0400

    Add Order struct

--- AdvanceProgrammingInSwift/main.swift
+++ AdvanceProgrammingInSwift/main.swift
@@ -247,3 +247,21 @@ do {
 } catch CalculatorError.divisionByZero {
     print("Division by zero detected and not allowed")
 }
+
+//Add Order struct
+struct Order {
+    let price: Int
+    let location: String
+}
+ 
+func totalRevenueOf(orders: [Order],  location: String) -> Int {
+    return 0
+}
+ 
+let orders = [
+    Order(price: 24, location: "New York"),
+    Order(price: 37, location: "San Francisco"),
+    Order(price: 101, location: "New York"),
+]
+ 
+let result = totalRevenueOf(orders: orders, location: "New York")
